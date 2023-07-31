0b86bff3c1f39f711cc10591ad38cbbe0f787946
GrossTal <66370873+GrossTal@users.noreply.github.com>
2023-07-31 05:27:27 +0300

    after first day

---
+++ src/Bg.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './Bg.css';
+
+function Bg() {
+  return (
+    <div className="Bg">
+      <div className="header">
+        <span className="header_text">העלאת תמונה כדי להסתיר את הרקע</span>
+        </div>
+        <bottun className="header_bt">העלאת תמונה</bottun>
+    </div>
+  );
+}
+
+export default Bg;
