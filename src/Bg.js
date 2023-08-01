2dc6d9b82a255474f1ef02b85fef06be7ae05dc8
GrossTal <66370873+GrossTal@users.noreply.github.com>
2023-08-01 18:03:19 +0300

    secound update

--- src/Bg.js
+++ src/Bg.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './Bg.css';
 
 function Bg() {
@@ -7,7 +6,22 @@ function Bg() {
       <div className="header">
         <span className="header_text">העלאת תמונה כדי להסתיר את הרקע</span>
         </div>
-        <bottun className="header_bt">העלאת תמונה</bottun>
+        <button className="header_bt">העלאת תמונה</button>
+        <span className="header_st">פורמטים נתמכים png, jpeg </span>
+        <div className = "main_div">
+            <div className="left">LEFT</div>
+            <div className="Right">
+                <div className ="middle_right_div">
+                <div className= "div_top">
+                  <div className = "top_div_top_text">תמונה חינם</div>  
+                  <div className = "top_div_bottom_text">תמונה מקדימה של התמונה 418x512</div> 
+                  <button className = "dw_b">הורד</button>
+                  <div className="right_div_top_sub_sub">איכות טובה עד 0.25 מגה פיקסל</div>
+                </div>
+                <div className= "div_bottom"></div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
